b68c3d364a1dd1fbdc81b58635271db8c008f16c
kyanesdev <kyanesdev@gmail.com>
2023-07-25 15:40:15 -0300

    Se agregaron los endpoints que ejecutan los scripts

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mqtt = require('mqtt');
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const { spawn } = require('child_process');
 
 const connectUrl = `${process.env.HOST}:${process.env.PORT_MOSQUITTO}`
 
@@ -34,9 +35,32 @@ let port = process.env.PORT || 5000;
 app.use(express.json());
 
 
-app.get('/recognition', (req, res) => {
-    res.send('Api de reconocimiento');
-  });
+app.post('/build-profile', (req, res) => {
+    const buildProfile = spawn('/bin/bash', ['-c', 'cd AI-face-recognition && source ./venv/bin/activate && pip install -r requirements.txt && python3 build_profile.py']);
+    buildProfile.on('close', (code) => {
+        if (code !== 0)
+            return res.status(500).json({status: 'failure', message: 'Error occurred in Python script'});
+        res.status(200).json({status: 'success', message: 'Python script ran successfully'});
+    });
+});
+
+app.post('/detect', (req, res) => {
+    const detect = spawn('/bin/bash', ['-c', 'cd AI-face-recognition && source ./venv/bin/activate && pip install -r requirements.txt && python3 detect.py']);
+    detect.on('close', (code) => {
+        if (code !== 0)
+            return res.status(500).json({status: 'failure', message: 'Error occurred in Python script'});
+        res.status(200).json({status: 'success', message: 'Python script ran successfully'});
+    });
+});
+
+app.post('/register-photo-profile', (req, res) => {
+    const registerPhotoProfile = spawn('/bin/bash', ['-c', 'cd AI-face-recognition && source ./venv/bin/activate && pip install -r requirements.txt && python3 register_photo_profile.py']);
+    registerPhotoProfile.on('close', (code) => {
+        if (code !== 0)
+            return res.status(500).json({status: 'failure', message: 'Error occurred in Python script'});
+        res.status(200).json({status: 'success', message: 'Python script ran successfully'});
+    });
+});
 
 app.listen(port, () => {
     console.log("Server Listening on PORT:", port); 
